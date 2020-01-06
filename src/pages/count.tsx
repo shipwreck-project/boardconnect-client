e425f455421d5e268637bb7cfd840c60ab265344
Godsenal <tmqps78@gmail.com>
2020-01-07 02:33:07 +0900

    :heavy_plus_sign: emotion 추가

--- src/pages/count.tsx
+++ src/pages/count.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increase, decrease, getRandomNumber } from 'store/count';
 import { NextPage } from 'next';
 import { NextJSContext } from 'next-redux-wrapper';
+import styled from '@emotion/styled';
+import { increase, decrease, getRandomNumber } from 'store/count';
 
 type Props = {
   isServer: boolean;
 };
+// style test
+const Button = styled.button`
+  width: 120px;
+  line-height: 30px;
+  background-color: white;
+  border: 1px solid black;
+  margin-right: 10px;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.5;
+  }
+`;
 
 const CountPage: NextPage<Props> = ({ isServer }) => {
   const { count, isLoading } = useSelector((state: StoreState) => state.count);
@@ -16,8 +30,8 @@ const CountPage: NextPage<Props> = ({ isServer }) => {
     <div>
       <h1>{`${text} 사이드 getInitialProps`}</h1>
       <h2>{isLoading ? '...loading' : count}</h2>
-      <button onClick={() => dispatch(increase())}>+</button>
-      <button onClick={() => dispatch(decrease())}>-</button>
+      <Button onClick={() => dispatch(increase())}>+</Button>
+      <Button onClick={() => dispatch(decrease())}>-</Button>
     </div>
   );
 };
